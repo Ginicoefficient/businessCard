a3e9155f92585846e742cb125cdcb305d303f9d3
Ginicoefficient <104734969+Ginicoefficient@users.noreply.github.com>
2023-03-21 16:39:32 -0500

    working on personal card

---
+++ business-card/src/Components/Body.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Body() {
+  return (
+    <div className="body-container">
+      <h3>About</h3>
+      <p>
+        I am a frontend developer with a specific interest in making every-day
+        tasks easy and automatic.
+      </p>
+      <h3>Interests</h3>
+      <p>
+        Craft art enthusiast. Cyclist and professional dog-walker. I spend my
+        free time playing card and board games with family, DND with friends,
+        and watching my favorite video game streamers.
+      </p>
+    </div>
+  );
+}
---
+++ business-card/src/Components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import selfPortrait from "/src/assets/self-portrait.png";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+const evelope = <FontAwesomeIcon icon={faEnvelope} />;
+const linkedIn = <FontAwesomeIcon icon={faLinkedin} />;
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="img-container">
+        <img className="portrait-img" src={selfPortrait} />
+      </div>
+      <h1>Ginny Mitchell</h1>
+      <p>Frontend Developer in Training</p>
+      <p>ginny.e.mitchell@gmail.com</p>
+      <div className="btn-container">
+        <button className="btn-email">{evelope} Email</button>
+        <button className="btn-linkdin">{linkedIn} LinkedIn</button>
+      </div>
+    </div>
+  );
+}
